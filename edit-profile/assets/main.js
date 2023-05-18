e97d500d69cd1ad9ea3f6daf8af12a903cf97cd9
Nguyen Quoc Hop <hop35889@gmail.com>
2023-05-18 14:24:02 +0700

    accesstoken

---
+++ edit-profile/assets/main.js
@@ -0,0 +1,48 @@
+async function getAll() {
+    return fetch('https://fakestoreapi.com/users')
+        .then(res => res.json())
+}
+
+async function fetchData() {
+    const users = await getAll();
+    console.log(users);
+
+
+}
+window.onload = function () {
+    fetchData();
+}
+
+function incorrectPasswordOrUsername() {
+    document.getElementById("incorrect-pass-username").classList.add("show-message");
+}
+
+async function login() {
+    console.log("login");
+    const userName = document.getElementById('login-name').value;
+    const userPass = document.getElementById('login-pass').value;
+
+    let accessToken = await fetch('https://dummyjson.com/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+
+            // username: 'kminchelle',
+            // password: '0lelplR',
+
+            username: userName,
+            password: userPass,
+        })
+    }).then(res => res.json()).then(data => data.token);
+
+
+    if(accessToken) {
+        console.log(accessToken);
+        window.localStorage.setItem('userToken', accessToken);
+        window.location = "/";
+    } else {
+
+        incorrectPasswordOrUsername();
+    }
+
+}
