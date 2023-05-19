3d0b6e3d09a3e20d962a4d3c8850782861915410
Nguyen Quoc Hop <hop35889@gmail.com>
2023-05-19 10:34:53 +0700

    user and pass

--- edit-profile/assets/edit-profile.js
+++ edit-profile/assets/edit-profile.js
@@ -10,3 +10,32 @@ function cancelEdit() {
         }
     }
 }
+
+
+const sidebar =  document.getElementsByClassName("sidebar")[0];
+const content = document.getElementsByClassName("content")[0];
+const aside = document.getElementsByClassName("aside")[0];
+const listSidebarItem = document.getElementsByClassName("sidebar-item");
+const listOpacity1 = document.getElementsByClassName("opacity1");
+
+function sidebarSetings() {
+    console.log("Se");
+
+    sidebar.classList.add('hideSideBar');
+    content.classList.add('positionOfContent');
+}
+
+aside.addEventListener("click", function(event) {
+    content.classList.remove('positionOfContent');
+    sidebar.classList.remove('hideSideBar');
+});
+
+for (let i = 0; i < listSidebarItem.length; i++) {
+    listSidebarItem[i].addEventListener('click', function(event) {
+
+        for(let x = 0; x < listOpacity1.length; x++){
+            listOpacity1[x].classList.remove('opacity1');
+        }
+        listSidebarItem[i].classList.add('opacity1');
+    });
+}
