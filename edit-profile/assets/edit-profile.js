c3fc43b516f3d497a3130211a74690282df5d499
Nguyen Quoc Hop <hop35889@gmail.com>
2023-05-19 08:43:42 +0700

    css form edit

--- edit-profile/assets/edit-profile.js
+++ edit-profile/assets/edit-profile.js
@@ -1,3 +1,12 @@
 window.onload = () => {
     console.log(document.URL)
 }
+
+function cancelEdit() {
+    let elements = document.getElementsByTagName("input");
+    for(let i = 0; i < elements.length; i++) {
+        if(elements[i].type == "text") {
+            elements[i].value = "";
+        }
+    }
+}
